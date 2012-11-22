aba5eb300f361907e9e71c0e12b33d8443f4ccb6
Dru Ibarra <dru@hyfn.com>
2012-11-22 00:53:23 -0800

    set description for index page

--- lib/index.rb
+++ lib/index.rb
@@ -1,5 +1,6 @@
 class App
   get "/" do
+    @description = "in-di.us provides independent mobile apps. We specialize in free customized apps for indie bands that earn you money."
     haml :index
   end
 end
