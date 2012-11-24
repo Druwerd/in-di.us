08f68c05087cf43eda18e3f5b4466c9c13ad491b
Dru Ibarra <dru@hyfn.com>
2012-11-23 16:17:47 -0800

    change index description

--- lib/index.rb
+++ lib/index.rb
@@ -1,6 +1,6 @@
 class App
   get "/" do
-    @description = "in-di.us provides independent mobile apps. We specialize in free customized apps for indie bands that earn you money."
+    @description = "in-di.us provides independent mobile apps. We specialize in free customized apps for indie bands."
     haml :index
   end
 end
