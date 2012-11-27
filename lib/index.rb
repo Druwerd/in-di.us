0dad2174724d2f096210990b032dde283ffe983a
Dru Ibarra <dru@hyfn.com>
2012-11-27 06:08:21 -0800

    add bootstrapp and "apps" page

--- lib/index.rb
+++ lib/index.rb
@@ -3,4 +3,9 @@ class App
     @description = "in-di.us provides independent mobile apps. We specialize in free customized apps for indie bands."
     haml :index
   end
+  
+  get "/apps" do
+    @description = "List of mobile apps created by in-di.us"
+    haml :apps
+  end
 end
