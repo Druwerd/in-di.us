ad0d1b0ae2fe75c5d244b03f170408dddbf79b8c
Dru Ibarra <dru@hyfn.com>
2012-11-29 07:29:33 -0800

    update page info

--- lib/index.rb
+++ lib/index.rb
@@ -1,20 +1,24 @@
 class App
   get "/" do
+    @title = "in-di.us - Independent Mobile Apps"
     @description = "in-di.us provides independent mobile apps. We specialize in free customized apps for indie bands."
     haml :index
   end
   
   get "/apps" do
+    @title = "in-di.us - Independent Mobile Apps"
     @description = "List of mobile apps created by in-di.us"
     haml :apps
   end
   
   get "/about" do
-    @description = "About in-di.us"
+    @title = "in-di.us - About Us"
+    @description = "We are the best hackers alive."
     haml :about
   end
   
   get "/contact" do
+    @title = "in-di.us - Contact Us"
     @description = "Contact information"
     haml :contact
   end
