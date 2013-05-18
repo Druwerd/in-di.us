7c8e0d0242e2c0c4550815f64af5a38819200f5f
Dru Ibarra <Druwerd@gmail.com>
2013-05-17 21:59:39 -0700

    add tests

--- spec/lib/resource_spec.rb
+++ spec/lib/resource_spec.rb
@@ -2,16 +2,37 @@ require File.join( File.dirname(__FILE__), '..', 'spec_helper')
 
 describe "Resource" do
   
-  it "should provide documentation" do
-    get '/resource.html'
+  it "should have a root" do
+    get '/'
+    last_response.should be_ok
+  end
+
+  it "should have about page" do
+    get '/about'
+    last_response.should be_ok
+  end
+
+  it "should have contact page" do
+    get '/contact'
     last_response.should be_ok
   end
   
-  it "should say hello world." do
-    get '/resource'
+  it "should say indi" do
+    get '/'
     last_response.should be_ok
-    resp = JSON.parse( last_response.body)
-    resp['hello'].should eq('world')
+    last_response.body.should include('indi')
+  end
+
+  it "should say about us" do
+    get '/about'
+    last_response.should be_ok
+    last_response.body.downcase.should include('about us')
+  end
+
+  it "should say contact us" do
+    get '/contact'
+    last_response.should be_ok
+    last_response.body.downcase.should include('contact us')
   end
   
 end
