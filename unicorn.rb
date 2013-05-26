aaf136261e26b8674b49c49bceca8de8a7b46186
Dru Ibarra <Druwerd@gmail.com>
2013-05-26 13:26:09 -0700

    decrease unicorn workers

--- unicorn.rb
+++ unicorn.rb
@@ -1,6 +1,6 @@
 #TMP_DIR = File.join( File.expand_path( File.dirname(__FILE__) ), 'tmp' )
 
-worker_processes 8
+worker_processes 4
 #working_directory File.dirname(__FILE__)
 
 # This loads the application in the master process before forking
